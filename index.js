066ef6901803b782e4f744c626e3bb233f8d4f91
Dfaye023 <70286830+Dfaye023@users.noreply.github.com>
2020-09-21 08:31:33 -0400

    Guess Project

--- index.js
+++ index.js
@@ -9,10 +9,65 @@ function ask(questionText) {
 
 start();
 
+function randomInt(min, max) {
+  let range = (max - min) + 1
+  let decimal = (Math.random() * range)
+  let int = Math.floor(decimal) + min
+
+  return int
+} // random number generator outside async function, to be called upon whenever
+
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+  let min = 1
+  let max = 100
+  let compGuess = randomInt(min, max)
+  
+  console.log("Let's play a game where you, puny human, make up a number and I, vastly superior computer, try to guess it. ")
+  let secretNumber = await ask("What is your secret number?\nPick a number between 1 & 100: ");
+  console.log('You entered: ' + secretNumber + ' ')
+  let winLoss = await ask(`Is your number ${compGuess}` + '? ')
+  if (winLoss === 'yes') { 
+    console.log('Damn, I\'m good! ')
+    process.exit()
+  } else // loop computer guesses. Reset min/max after every guess
+    while (winLoss === 'no') {
+       
+      if (parseInt(secretNumber) === compGuess && winLoss === 'no') { // parseInt turned secretNumber into an actual number not string
+        console.log('Don\'t lie! Rebel scum...')
+        process.exit()
+      }
+      let highLow = await ask('Is it higher or lower? ')
+      if (secretNumber > compGuess && highLow === 'lower') {
+        console.log('Don\'t lie! Rebel scum...')
+        process.exit()
+      }
+      if (secretNumber < compGuess && highLow === 'higher') {
+        console.log('Don\'t lie! Rebel scum...')
+        process.exit()
+      }
+      if (highLow === 'higher') {
+        min = compGuess + 1
+        compGuess = randomInt(min, max)
+      } else if (highLow === 'lower') {
+        max = compGuess - 1
+        compGuess = randomInt(min, max)
+      } else {
+        console.log('No, no, no.... try again!')
+      }
+      winLoss = await ask(`Is your number ${compGuess}` + '? ')
+      if (winLoss === 'yes') {
+        console.log('Finally!!')
+        process.exit()
+      }
+
+
+    }
+
 }
+
+
+
+
+
+
+
